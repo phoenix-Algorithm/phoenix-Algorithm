960963275848a4933e7fadfd19a5f749540edb57
yangjaejun <yang7134@gmail.com>
2023-07-18 08:05:02 +0900

    [JCT] 문자열/2023.07.18(화)/1

---
+++ 양재준/문자열/[JCT]_1152.js
@@ -0,0 +1,16 @@
+let fs = require('fs');
+let input = fs.readFileSync('dev/stdin').toString().split('\n');
+
+let str = input[0].trim().split(' ');
+
+if(str == '') console.log(0);
+else console.log(str.length);
+
+
+
+
+
+
+
+
+
