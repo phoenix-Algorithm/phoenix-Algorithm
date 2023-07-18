108399519b7297d5ee4bf356e38fe3dc27942b8e
jisu Seo <jseo9732@gmail.com>
2023-07-18 15:59:52 +0900

    [BOJ] 조건문 / 2023.07.14(금) / 1

---
+++ 서지수/조건문/[BOJ]_14681.js
@@ -0,0 +1,21 @@
+// 사분면 고르기
+
+let fs = require("fs");
+let input = fs.readFileSync(0).toString().split("\n");
+
+const x = Number(input[0]);
+const y = Number(input[1]);
+
+if (x < 0) {
+  if (y < 0) {
+    console.log(3);
+  } else {
+    console.log(2);
+  }
+} else {
+  if (y < 0) {
+    console.log(4);
+  } else {
+    console.log(1);
+  }
+}
