c1984f3238d3306000990eb5101b6735ebc385fc
89882 <pgh123166@naver.com>
2023-08-13 23:26:23 +0900

    comment: 주석 추가

--- 박가현/그리디/[JCT]_11059.js
+++ 박가현/그리디/[JCT]_11059.js
@@ -1,3 +1,21 @@
+// 제 코드인데 강의 코드랑 obj가 객체냐 배열이냐 차이인데 오답인 이유를 모르겠어요
+// let fs = require('fs');
+// let path = process.platform === 'linux' ? '/dev/stdin' : 'test.txt';
+// let [n, input] = fs.readFileSync(path).toString().trim().split('\n');
+// let array = input.split(' ').map(Number);
+// let obj = {};
+// let count = 0;
+// for (let i of array) {
+//   if (obj[i] > 0) {
+//     obj[i] -= 1;
+//   } //화살이 새로 필요한거
+//   else {
+//     count++;
+//   } //화살이 존재함
+//   obj[i - 1] += 1;
+// }
+// console.log(count);
+
 //강의 정답 코드입니다
 const rl = require('readline').createInterface({
   input: process.stdin,
