07b8dcd94569168d95eff0eac6f3b42f9b21eaf7
89882 <pgh123166@naver.com>
2023-08-13 23:28:25 +0900

    [JCT] 그리디 / 2023.08.14(월) / 2

---
+++ 박가현/그리디/[JCT]_19939.js
@@ -0,0 +1,16 @@
+let fs = require('fs');
+let path = process.platform === 'linux' ? '/dev/stdin' : 'test.txt';
+let [n, input] = fs.readFileSync(path).toString().trim().split('\n');
+let array = input.split(' ').map(Number);
+let obj = {};
+let count = 0;
+for (let i of array) {
+  if (obj[i] > 0) {
+    obj[i] -= 1;
+  } //화살이 새로 필요한거
+  else {
+    count++;
+  } //화살이 존재함
+  obj[i - 1] += 1;
+}
+console.log(count);
