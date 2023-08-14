0b48521ccd67d49a00f96a76e76341cdef08c866
yangjaejun <yang7134@gmail.com>
2023-08-14 10:21:06 +0900

    fix 19939.js 로직 수정 및 강의 풀이 추가

--- 양재준/그리디/[JCT]_19939.js
+++ 양재준/그리디/[JCT]_19939.js
@@ -3,23 +3,39 @@ let [N, K] = fs.readFileSync('dev/stdin').toString().split(' ').map(Number);
 
 // N:6, K:3 일때
 // 1 2 3
+// => 2
 // N:7, K:3 일때
-// 1 2 3+1
+// 1 2 4
+// => 3
 // N:8, K:3 일때
-// 1 2+1 4+1
+// 1 3 4
+// => 3
 // N:9, K:3 일때
-// 1+1 2+1 3+1
+// 2 3 4
+// => 2
 
-// N이 K의 배수이면 연속된 개수로 분배
-// N이 K의 배수가 아니면 나머지를 1씩 분배
-// 그러다 나머지가 0이 될때 N은 K의 배수가 됨
-
-// 1. 1부터 K까지의 합이 N보다 작으면 -1
-// 2. N이 K의 배수이면 K-1
-// 3. N이 K의 배수가 아니면 K
 function solution(N, K) {
-  if (N < (K * (K + 1)) / 2) return -1;
-  return N % K === 0 ? K - 1 : K;
+  const sum = (K * (K + 1)) / 2;
+  if (N < sum) return -1;
+  N -= sum; // 이미 담은 공의 개수를 뺀 나머지 공의 개수를 계산
+  return N % K === 0 ? K - 1 : K; // 남은 공을 바구니에 분배
 }
 
 console.log(solution(N, K));
+
+/* 강의 풀이 */
+/* let summary = 0;
+for (let i = 1; i < K + 1; i++) {
+  summary += i;
+}
+if (summary > N) {
+  console.log(-1);
+} else {
+  N -= summary;
+  if (N % K === 0) {
+    console.log(K - 1);
+  } else {
+    console.log(K);
+  }
+}
+ */
