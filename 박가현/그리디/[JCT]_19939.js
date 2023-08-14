c0e00387f97c09a4f9e45bab5ba9e6089e0f4452
89882 <pgh123166@naver.com>
2023-08-14 14:43:17 +0900

    fix: 19939 코드 수정

--- 박가현/그리디/[JCT]_19939.js
+++ 박가현/그리디/[JCT]_19939.js
@@ -1,16 +1,17 @@
 let fs = require('fs');
 let path = process.platform === 'linux' ? '/dev/stdin' : 'test.txt';
-let [n, input] = fs.readFileSync(path).toString().trim().split('\n');
-let array = input.split(' ').map(Number);
-let obj = {};
-let count = 0;
-for (let i of array) {
-  if (obj[i] > 0) {
-    obj[i] -= 1;
-  } //화살이 새로 필요한거
-  else {
-    count++;
-  } //화살이 존재함
-  obj[i - 1] += 1;
+let input = fs.readFileSync(path).toString().trim().split(' ').map(Number);
+let n = input[0];
+let k = input[1];
+
+let num = ((k + 1) * k) / 2; // 등차수열 합 공식
+if (n < num) {
+  console.log(-1); // 등차수열 합 보다도 작으면 -1
+} else {
+  let a = n - num;
+  if (a % k === 0) {
+    console.log(k - 1);
+  } else {
+    console.log(k);
+  }
 }
-console.log(count);
