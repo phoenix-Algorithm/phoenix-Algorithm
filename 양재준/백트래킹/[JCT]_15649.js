f64731253cbe733e24cdc05566049b38db922e77
yangjaejun <yang7134@gmail.com>
2023-08-28 02:50:38 +0900

    [JCT] 백트래킹 / 2023.08.29(화) / 2

---
+++ 양재준/백트래킹/[JCT]_15649.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const [N, M] = fs.readFileSync('dev/stdin').toString().split(' ').map(Number);
+
+let seq = [];
+let visited = Array(N).fill(false);
+let answer = [];
+
+function dfs(depth) {
+  if (depth === M) {
+    answer += seq.join(' ') + '\n';
+    return;
+  }
+
+  for (let i = 0; i < N; i++) {
+    if (visited[i]) continue;
+    visited[i] = true;
+    seq.push(i + 1);
+    dfs(depth + 1);
+    seq.pop();
+    visited[i] = false;
+  }
+}
+
+dfs(0);
+console.log(answer);
